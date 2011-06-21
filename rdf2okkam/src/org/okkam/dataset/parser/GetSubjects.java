367b3608ade19d3976e1b359c86710e5a2e56be7
seidymam@gmail.com <seidymam@gmail.com@75b3a071-75c7-782f-4d73-79cb31a87847>
2011-06-21 16:21:57 +0000

    Update to Get Subects with Blank Node Object

--- rdf2okkam/src/org/okkam/dataset/parser/GetSubjects.java
+++ rdf2okkam/src/org/okkam/dataset/parser/GetSubjects.java
@@ -1,5 +1,4 @@
 package org.okkam.dataset.parser;
-
 import com.hp.hpl.jena.rdf.model.*;
 import com.hp.hpl.jena.util.FileManager;
 import java.io.*;
@@ -8,20 +7,28 @@ import java.util.Iterator;
 import java.util.Set;
 /**
  * This program returns all subjects which have literal or URI value fir each RDF statement.
- *@author Seid
+ *
  */
 public class GetSubjects extends Object {
+	
+	
+	static Model model = null;
+	
 	public static void main(String args[]) throws IOException {
+		 String rdfNS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 		 String inputFileName = "resources/anagrafe1.ttl";
 		 Iterator it=getSubjects(inputFileName).iterator();
+		 Property type = model.getProperty(rdfNS + "type");
 		 int i=0;
 		 while(it.hasNext()){
-			 System.out.println(++i+": "+it.next()+"\n");
+			 Resource subject = (Resource) it.next();
+			 Statement stmt = model.getProperty(subject, type);
+			 System.out.println(++i+": "+ subject + " , object " + stmt.getObject().toString() + "\n");
 		 }
 	}
 	public static Set getSubjects(String filePath){
 		// create an empty model
-		Model model = ModelFactory.createDefaultModel();
+		model = ModelFactory.createDefaultModel();
 
 		// use the FileManager to find the input file
 		InputStream in = FileManager.get().open(filePath);
@@ -34,17 +41,26 @@ public class GetSubjects extends Object {
 		// select all the subjects with literal or uri value
 		StmtIterator iter = model.listStatements();
 		Set  subjects = new HashSet();
+		Set  tempSubjects = new HashSet();
 		if (iter.hasNext()) {
 			while (iter.hasNext()) {
 				Statement stmt = iter.nextStatement(); // get next statement
 				Resource subject = stmt.getSubject(); // get the subject
 				RDFNode object = stmt.getObject(); // get the object
 				//if the object is not anonymous/Blank node, return the subject
-				if (!object.isAnon())
+				if (object.isAnon())
+					tempSubjects.add(subject);
+				else 
+					{
 					subjects.add(subject);
+					}
 			}
-		} else {
-			return subjects;
+		} 
+		Iterator subit=tempSubjects.iterator();
+		while(subit.hasNext()){
+			RDFNode object =(RDFNode) subit.next();
+			if(subjects.contains(object))
+				subjects.remove(object);
 		}
 		return subjects;
 	}
