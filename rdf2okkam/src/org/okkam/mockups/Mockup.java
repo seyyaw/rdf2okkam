e36d3d96e1703fd68cf8402c089dabb70136d2db
luigiselmi@gmail.com <luigiselmi@gmail.com@75b3a071-75c7-782f-4d73-79cb31a87847>
2011-06-27 15:36:27 +0000

    changed name from a longer one

---
+++ rdf2okkam/src/org/okkam/mockups/Mockup.java
@@ -0,0 +1,112 @@
+package org.okkam.mockups;
+import org.okkam.dataset.parser.GetSubjects;
+import java.util.Iterator;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.update.UpdateAction;
+import com.hp.hpl.jena.update.UpdateFactory;
+import com.hp.hpl.jena.update.UpdateRequest;
+import com.hp.hpl.jena.util.FileManager;
+import com.hp.hpl.jena.query.*;
+
+/**
+ * This Class is used to Retrieve Okkam ID from ENS repository and modify the
+ * RDF repository Accordingly.
+ * 
+ */
+public class Mockup {
+	static Model model = null;
+	static String inputFileName = "resources/anagrafe1.ttl";
+	static String inputFileName2="resources/mockup.ttl";
+
+	public static void main(String[] args) throws Exception {
+		loadModel(inputFileName);		
+		GetSubjects.loadModel(inputFileName);
+		modifyRDF();
+	}
+	/**
+	 * This Function is Used to get Okkam ID from the ENS repository and return
+	 * array of subjects, properties and objects accordingly
+	 * 
+	 * @param statment
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	public static String[][] getOkkamId(String[][] statment)
+			throws FileNotFoundException {
+		for (int i = 0; i < GetSubjects.getsize(); i++) {
+			String subject = statment[i][0], property = statment[i][1], object = statment[i][2];
+				object = "'" + object + "'";
+			Query SPARQL_QUERY = QueryFactory
+					.create("prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+							+ "prefix ens: <http://models.okkam.org/ENS-core-vocabulary.owl#>  "
+							+ "SELECT ?okkamid "
+							+ "WHERE {?okkamid <"
+							+ property + "> " + object + " . }");
+			InputStream in = new FileInputStream(new File(
+					"resources/mockup.ttl"));
+			Model model = ModelFactory.createDefaultModel();
+			model.read(in, null, "TURTLE");
+			QueryExecution qe = QueryExecutionFactory.create(SPARQL_QUERY,
+					model);
+			try {
+				ResultSet results = qe.execSelect();
+				while (results.hasNext()) {
+					QuerySolution result = results.next();
+					statment[i][0] = result.get("okkamid").toString();
+				}
+			} finally {
+				qe.close();
+			}
+		}
+		return statment;
+	}
+
+	/**
+	 * A function that will modify the RDF dataset based on the retrieved Okkam
+	 * ID from the ENS Repository
+	 * 
+	 * @throws FileNotFoundException
+	 */
+	public static void modifyRDF() throws FileNotFoundException {
+		File outrdf = new File("resources/test.ttl");
+	    OutputStream out = new FileOutputStream(outrdf); 
+		Iterator it = GetSubjects.getSubjects(inputFileName).iterator();
+		String[][] statment = GetSubjects.getProperties(it);
+		String[][] stmt = getOkkamId(statment);
+		for (int i = 0; i < GetSubjects.getsize(); i++) {
+			String subject = stmt[i][0], property = stmt[i][1], object = stmt[i][2];
+				object = "'" + object + "'";
+			UpdateRequest request2 = UpdateFactory.create("DELETE WHERE { ?x <"
+					+ property + "> " + object + " }");
+			// And perform the operations.
+			request2 = UpdateFactory.create("INSERT DATA { <" + subject + "> <"
+					+ property + "> " + object + " }");
+			UpdateAction.execute(request2, model);
+		}
+		model.write(System.out, "TTL");
+		//Write the RDF/TTL file to filesystem
+		model.write(out,"TTL");
+	}
+
+	public static void loadModel(String filePath) {
+		// create an empty model
+		model = ModelFactory.createDefaultModel();
+
+		// use the FileManager to find the input file
+		InputStream in = FileManager.get().open(filePath);
+		if (in == null) {
+			throw new IllegalArgumentException("File: " + filePath
+					+ " not found");
+		}
+		// read the RDF/TTL file
+		model.read(in, null, "TURTLE");
+	}
+}
