61bc85cce79f1139ecaa7013f4ae90c2a829b18e
luigiselmi@gmail.com <luigiselmi@gmail.com@75b3a071-75c7-782f-4d73-79cb31a87847>
2011-06-28 15:41:27 +0000

    added function to get and remove duplicates

--- rdf2okkam/test/org/okkam/rdf/parser/RdfParserTest.java
+++ rdf2okkam/test/org/okkam/rdf/parser/RdfParserTest.java
@@ -13,13 +13,19 @@ import org.junit.Test;
 import org.okkam.client.data.AttributeType;
 import org.okkam.client.data.AttributesType;
 import org.okkam.dataset.parser.RdfParser;
+import org.okkam.exception.SameNodeException;
 import org.okkam.model.ModelLoader;
 
 import com.hp.hpl.jena.ontology.DatatypeProperty;
 import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Selector;
+import com.hp.hpl.jena.rdf.model.SimpleSelector;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 public class RdfParserTest {
 	
@@ -190,5 +196,114 @@ public class RdfParserTest {
 		}
 	}
 	
+	@Test
+	public void testGetStrictSubject() {
+		System.out.println("---------------Test getStrictSubjects()----------------") ;
+		Set<RDFNode> subjects = parser.getStrictSubjects() ;
+		System.out.println("Number of strict subject node: " + subjects.size()) ;
+		Iterator<RDFNode> i = subjects.iterator() ;
+		int count = 0 ;
+		while(i.hasNext()) {
+			RDFNode subject = i.next() ;
+			count++ ;
+			System.out.println(count + ") strict subject node: " + subject.toString()) ;
+			AttributesType attributesType = parser.listSubjectProperties(subject);
+			List<AttributeType> attributes = attributesType.getAttributes();
+			Iterator<AttributeType> iattributes = attributes.iterator() ;						
+			while(iattributes.hasNext()){
+				AttributeType attribute = iattributes.next();
+				String attributeName = attribute.getName().getLocalPart();
+				String prefix = attribute.getName().getPrefix();
+				String attributeValue = attribute.getValue();
+				System.out.println("Attribute: " + attributeName + ", prefix: " + prefix + ", value: " + attributeValue);
+			}
+		}
+		
+	}
+	
+	@Test
+	public void testGetNotStrictSubjects() {
+		System.out.println("---------------Test getNotStrictSubjects()----------------") ;
+		Set<RDFNode> subjects = parser.getNotStrictSubjects() ;
+		System.out.println("Number of not strict subject node: " + subjects.size()) ;
+		Iterator<RDFNode> i = subjects.iterator() ;
+		int count = 0 ;
+		while(i.hasNext()) {
+			RDFNode subject = i.next() ;
+			count++ ;
+			System.out.println(count + ") not strict subject node: " + subject.toString()) ;
+			AttributesType attributesType = parser.listSubjectProperties(subject);
+			List<AttributeType> attributes = attributesType.getAttributes();
+			Iterator<AttributeType> iattributes = attributes.iterator() ;						
+			while(iattributes.hasNext()){
+				AttributeType attribute = iattributes.next();
+				String attributeName = attribute.getName().getLocalPart();
+				String prefix = attribute.getName().getPrefix();
+				String attributeValue = attribute.getValue();
+				System.out.println("Attribute: " + attributeName + ", prefix: " + prefix + ", value: " + attributeValue);
+			}
+		}
+		
+	}
+	
+	@Test
+	public void testCompareSubjects() {
+		System.out.println("---------------Test compareSubjects()----------------") ;
+		final String ensNS = "http://models.okkam.org/ENS-core-vocabulary.owl#" ; 
+		Property subj1Predicate = loader.getInputModel().getProperty(ensNS + "location_name") ;		
+		Selector selector1 = new SimpleSelector(null, subj1Predicate, "FOLGARIA" ) ;
+		StmtIterator isubj1 = loader.getInputModel().listStatements(selector1) ;
+		while(isubj1.hasNext()) {
+			Statement stmt1 = isubj1.next() ;
+			RDFNode subj1 = stmt1.getSubject() ;
+			Selector selector2 = new SimpleSelector(null, subj1Predicate, (RDFNode) null) ;
+			StmtIterator isubj2 = loader.getInputModel().listStatements(selector2) ;
+			while(isubj2.hasNext()) {
+				Statement stmt2 = isubj2.next() ;
+				RDFNode subj2 = stmt2.getSubject() ;
+				
+				if( parser.compareSubjects(subj1, subj2) ) 
+					System.out.println( stmt1.toString() + " same as " + stmt2.toString()) ; 
+				else
+					System.out.println( stmt1.toString() + " different from " + stmt2.toString()) ;
+				 
+			}
+		}
+						
+	}
+	
+	@Test
+	public void testGetDuplicateSubjects() {
+		System.out.println("---------------Test getDuplicateSubjects()----------------") ;
+		final String ensNS = "http://models.okkam.org/ENS-core-vocabulary.owl#" ; 
+		Property subj1Predicate = loader.getInputModel().getProperty(ensNS + "location_name") ;		
+		Selector selector1 = new SimpleSelector(null, subj1Predicate, "FOLGARIA" ) ;
+		StmtIterator isubj1 = loader.getInputModel().listStatements(selector1) ;
+		Set<RDFNode> duplicates = null ;
+		RDFNode subj1 = isubj1.next().getSubject() ;		
+		Statement stmt1 = isubj1.next() ;					
+		duplicates = parser.getDuplicateSubjects(subj1) ;				
+		Iterator<RDFNode> i = duplicates.iterator() ;
+		while(i.hasNext()) {
+			RDFNode duplicate = i.next() ;
+			System.out.println(subj1 + " has " + duplicate + " as a duplicate resource.") ;
+		}
+		
+	}
+	
+	@Test
+	public void testRemoveDuplicates() {
+		System.out.println("---------------Test removeDuplicateS()----------------") ;
+		final String ensNS = "http://models.okkam.org/ENS-core-vocabulary.owl#" ; 
+		Property subj1Predicate = loader.getInputModel().getProperty(ensNS + "location_name") ;		
+		Selector selector1 = new SimpleSelector(null, subj1Predicate, "FOLGARIA" ) ;
+		StmtIterator isubj1 = loader.getInputModel().listStatements(selector1) ;
+		Statement stmt = isubj1.next() ;
+		System.out.println("Statement: " + stmt ) ;
+		RDFNode subj1 = stmt.getSubject() ;
+		Model resultModel = parser.removeDuplicates(subj1) ;
+		resultModel.write(System.out, "TURTLE") ;
+		
+	}
 
 }
