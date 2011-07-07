48569f2cfaad0c657854a347c304cf70f3beb370
luigiselmi@gmail.com <luigiselmi@gmail.com@75b3a071-75c7-782f-4d73-79cb31a87847>
2011-07-07 09:45:43 +0000

    changed name to some packages and classes

---
+++ rdf2okkam/src/it/okkam/rdf2okkam/controller/ApplicationController.java
@@ -0,0 +1,47 @@
+package it.okkam.rdf2okkam.controller;
+
+import org.okkam.client.data.AttributesType;
+import org.okkam.client.data.ProfileType;
+import org.okkam.dataset.parser.EntityBuilder;
+import org.okkam.service.client.OkkamClient;
+import org.okkam.service.client.ServiceClient;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+
+public class ApplicationController {
+	
+	private final String confpath = "conf";
+	
+	Model _model = null ;
+	
+	EntityBuilder builder = null ;
+	
+	OkkamClient okkam = null;
+	
+	ServiceClient service = null ;
+	
+	public ApplicationController(Model model) {
+		_model = model ;
+		builder = new EntityBuilder( _model ) ;
+		okkam = new OkkamClient( confpath ) ;
+		service = new ServiceClient( okkam ) ;
+	}
+	
+	public String createEntity(RDFNode node) {
+		
+		String result = "" ; // okkamid
+		
+		ProfileType profile = builder.buildEntity( node ) ;
+		
+		boolean ignoreDuplicates = false ;
+		
+		
+		result = service.createNewEntity(profile.getSemanticType(), 
+				profile.getAttributes(), ignoreDuplicates) ;
+				
+		
+		return result ;
+	}
+
+}
