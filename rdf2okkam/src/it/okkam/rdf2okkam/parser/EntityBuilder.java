814cda01f0fec49c685572ee25c26c89ea3154d3
luigiselmi@gmail.com <luigiselmi@gmail.com@75b3a071-75c7-782f-4d73-79cb31a87847>
2011-07-07 09:57:38 +0000

    changed name to some packages and classes

---
+++ rdf2okkam/src/it/okkam/rdf2okkam/parser/EntityBuilder.java
@@ -0,0 +1,74 @@
+package it.okkam.rdf2okkam.parser;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.okkam.client.data.AttributesType;
+import org.okkam.client.data.ProfileType;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.rdf.model.Selector;
+import com.hp.hpl.jena.rdf.model.SimpleSelector;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+
+public class EntityBuilder {
+	
+	private final String rdfNS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+	private final String RDF_TYPE = rdfNS + "type" ;
+	private final String ensNS = "http://models.okkam.org/ENS-core-vocabulary.owl#";
+	private final String ENS_LOCATION = ensNS + "location" ;
+	private final String ENS_PERSON = ensNS + "person" ;
+	
+	Model _model = null ;
+	
+	private static Log log = LogFactory.getLog(EntityBuilder.class);
+	
+	public EntityBuilder(Model model) {
+		_model = model ;
+	}
+	
+	EntityBuildContext context = null ;
+	
+	public ProfileType buildEntity(RDFNode subjectNode) {
+		
+		Resource subject = subjectNode.asResource() ;
+		
+		context = new EntityBuildContext( _model ) ;
+		
+		String subjectType = getType( subjectNode ) ;
+		
+		if( ENS_LOCATION.equals( subjectType ) ) {
+			//if subject is a location
+			context.setLocationEntityBuilding() ;
+		
+		}
+		if( ENS_PERSON.equals( subjectType ) ) {
+			//if subject is a person
+			context.setPersonEntityBuilding() ;
+		}
+		
+		log.info(subject + " is a " + subjectType ) ;
+		
+		return context.getProperties(subjectNode) ;
+		
+	}
+	
+	private String getType(RDFNode subjectNode) {
+		String result = null ;
+		Property rdfType = ResourceFactory.createProperty(RDF_TYPE) ;
+		Selector selector = new SimpleSelector(subjectNode.asResource(), rdfType, (RDFNode) null) ;
+		StmtIterator istmt = _model.listStatements(selector) ;
+		while(istmt.hasNext()) {
+			Statement stmt = istmt.next() ;
+			RDFNode object = stmt.getObject() ;
+			if( ensNS.equals( object.asResource().getNameSpace() ) )
+				result = object.asResource().getURI() ;
+		}
+		return result ;
+	}
+
+}
---
+++ rdf2okkam/src/it/okkam/rdf2okkam/parser/RdfImportController.java
@@ -0,0 +1,34 @@
+package it.okkam.rdf2okkam.parser;
+
+import org.okkam.service.client.OkkamClient;
+import org.okkam.service.client.ServiceClient;
+
+import com.hp.hpl.jena.rdf.model.Model;
+
+public class RdfImportController {
+
+	private static final String filename = "resources/anagrafe-unreconciled.ttl";
+	
+	private static final String confpath = "conf";
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Model modelIn = null ;
+		
+		Model modelOut = null ;
+		
+		RdfUtil parser = new RdfUtil(modelIn, modelOut);
+		
+		ServiceClient client = new ServiceClient(new OkkamClient(confpath));
+		
+		client.createNewEntity(null,null, false);
+	
+
+	}
+
+}
