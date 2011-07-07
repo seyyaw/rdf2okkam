895bc3d0aeba4d32d7b18988d46ec03786bf0d65
luigiselmi@gmail.com <luigiselmi@gmail.com@75b3a071-75c7-782f-4d73-79cb31a87847>
2011-07-07 13:08:46 +0000

    changed name to some packages and classes

---
+++ rdf2okkam/test/it/okkam/rdf2okkam/ens/EntityBuilderTest.java
@@ -0,0 +1,97 @@
+package it.okkam.rdf2okkam.ens;
+
+import static org.junit.Assert.*;
+
+import it.okkam.rdf2okkam.ens.EntityBuilder;
+import it.okkam.rdf2okkam.parser.Globalizer;
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.okkam.client.data.AttributeType;
+import org.okkam.client.data.AttributesType;
+import org.okkam.client.data.ProfileType;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.util.FileManager;
+
+public class EntityBuilderTest {
+	
+	private final String ensNS = "http://models.okkam.org/ENS-core-vocabulary.owl#";
+	private final String ENS_LOCATION = ensNS + "location" ;
+	private final String rdfNS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+	private final String RDF_TYPE = rdfNS + "type" ;
+	String modelFileName = "resources/dataset_out.ttl" ;
+	final String RDF_SYNTAX = "TURTLE" ;
+	String baseUri = null ;
+	Model _model = null ;
+	Model _outModel = null ;
+	EntityBuilder builder = null ;
+	Globalizer global = null ;
+	
+	private static Log log = LogFactory.getLog(EntityBuilderTest.class);
+
+	@Before
+	public void setUp() throws Exception {
+		
+		// use the FileManager to find the input file
+		InputStream inModelStream = FileManager.get().open( modelFileName );
+		if (inModelStream == null) {		    
+			log.error("File: " + modelFileName + " not found");
+			System.exit(0);
+		}
+
+		// Create the input model. Models different from the default one import also the 
+		// rdf and rdf-schema axioms. 
+		_model = ModelFactory.createDefaultModel();
+		
+		// read the RDF/TURTLE file
+		_model.read(inModelStream, baseUri, "TURTLE");
+		
+		// create the output model
+		_outModel = ModelFactory.createDefaultModel() ;	
+		
+		builder = new EntityBuilder(_model) ;
+		
+		global = new Globalizer(_model, _outModel) ;
+		
+	}
+
+	@Test
+	public void testBuildEntity() {
+		log.info("----------testGetProperties--------------") ;
+		
+		Set<RDFNode> distSubjs = global.getDistinctSubjects() ;
+		
+		Iterator<RDFNode> idistSubj = distSubjs.iterator() ;
+		while(idistSubj.hasNext()) {
+			RDFNode distSubj = idistSubj.next() ;
+			
+			ProfileType profile = null;
+			profile = builder.buildEntity(distSubj);
+			List<AttributeType> attrTypes = profile.getAttributes().getAttributes() ;
+			Iterator<AttributeType> iattrType = attrTypes.iterator() ;
+			while(iattrType.hasNext()) {
+				AttributeType attrType = iattrType.next() ;
+				QName name = attrType.getName() ;
+				String value = attrType.getValue() ;
+				log.info( distSubj + ", " + name.getPrefix() + ":" + name.getLocalPart() + ", " + value) ;
+			}
+			
+		}
+	}
+
+}
---
+++ rdf2okkam/test/it/okkam/rdf2okkam/ens/LocationTest.java
@@ -0,0 +1,98 @@
+package it.okkam.rdf2okkam.ens;
+
+import static org.junit.Assert.*;
+
+import it.okkam.rdf2okkam.ens.Location;
+import it.okkam.rdf2okkam.parser.Globalizer;
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.xml.namespace.QName;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.okkam.client.data.AttributeType;
+import org.okkam.client.data.AttributesType;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.util.FileManager;
+
+public class LocationTest {
+	private final String ensNS = "http://models.okkam.org/ENS-core-vocabulary.owl#";
+	private final String ENS_LOCATION = ensNS + "location" ;
+	private final String rdfNS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+	private final String RDF_TYPE = rdfNS + "type" ;
+	String modelFileName = "resources/dataset_out.ttl" ;
+	final String RDF_SYNTAX = "TURTLE" ;
+	String baseUri = null ;
+	Model _model = null ;
+	Model _outModel = null ;
+	Location location = null ;
+	Globalizer global = null ;
+	
+	private static Log log = LogFactory.getLog(LocationTest.class);
+	
+	@Before
+	public void setUp() throws Exception {
+		// use the FileManager to find the input file
+		InputStream inModelStream = FileManager.get().open( modelFileName );
+		if (inModelStream == null) {		    
+			log.error("File: " + modelFileName + " not found");
+			System.exit(0);
+		}
+
+		// Create the input model. Models different from the default one import also the 
+		// rdf and rdf-schema axioms. 
+		_model = ModelFactory.createDefaultModel();
+		
+		// read the RDF/TURTLE file
+		_model.read(inModelStream, baseUri, "TURTLE");
+		
+		// create the output model
+		_outModel = ModelFactory.createDefaultModel() ;	
+		
+		location = new Location(_model) ;
+		
+		global = new Globalizer(_model, _outModel) ;
+	}
+
+	@Test
+	public void testGetProperties() {
+		log.info("----------testGetProperties--------------") ;
+		Resource locationType = ResourceFactory.createResource( ENS_LOCATION ) ;
+		Property rdfType = ResourceFactory.createProperty(RDF_TYPE) ;
+		Set<RDFNode> distSubjs = global.getDistinctSubjects() ;
+		
+		Iterator<RDFNode> idistSubj = distSubjs.iterator() ;
+		while(idistSubj.hasNext()) {
+			RDFNode distSubj = idistSubj.next() ;
+			if(distSubj.asResource().hasProperty(rdfType, locationType)) {
+				AttributesType attrsTypes = null;
+				attrsTypes = location.getProperties(distSubj).getAttributes();
+				List<AttributeType> attrTypes = attrsTypes.getAttributes() ;
+				Iterator<AttributeType> iattrType = attrTypes.iterator() ;
+				while(iattrType.hasNext()) {
+					AttributeType attrType = iattrType.next() ;
+					QName name = attrType.getName() ;
+					String value = attrType.getValue() ;
+					log.info( distSubj + ", " + name.getPrefix() + ", " + name.getNamespaceURI() + ", " + value) ;
+				}
+			}
+			else {
+				log.info( distSubj + " is not a location") ;
+			}
+		}
+		
+	}
+
+}
