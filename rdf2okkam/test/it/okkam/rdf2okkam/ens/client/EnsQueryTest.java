ca1542b5a079e1fe08dc8c31f5f058f67635c712
luigiselmi@gmail.com <luigiselmi@gmail.com@75b3a071-75c7-782f-4d73-79cb31a87847>
2011-07-07 13:29:34 +0000

    changed name to some packages and classes

---
+++ rdf2okkam/test/it/okkam/rdf2okkam/ens/client/EnsQueryTest.java
@@ -0,0 +1,19 @@
+package it.okkam.rdf2okkam.ens.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EnsQueryTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetQuery() {
+		fail("Not yet implemented");
+	}
+
+}
