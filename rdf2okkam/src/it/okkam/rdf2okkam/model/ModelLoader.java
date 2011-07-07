c9c896990892f6332bddec551f53ffba5126945d
luigiselmi@gmail.com <luigiselmi@gmail.com@75b3a071-75c7-782f-4d73-79cb31a87847>
2011-07-07 12:22:21 +0000

    changed name to some packages and classes

--- rdf2okkam/src/it/okkam/rdf2okkam/model/ModelLoader.java
+++ rdf2okkam/src/it/okkam/rdf2okkam/model/ModelLoader.java
@@ -1,5 +1,7 @@
 package it.okkam.rdf2okkam.model;
 
+import it.okkam.rdf2okkam.ens.client.ServiceClient;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -8,7 +10,6 @@ import java.io.OutputStream;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.okkam.service.client.ServiceClient;
 
 import com.hp.hpl.jena.ontology.OntDocumentManager;
 import com.hp.hpl.jena.ontology.OntModel;
